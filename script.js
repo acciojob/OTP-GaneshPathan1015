51a8bd948a1ccba15d68204a8a4ac4fe25a37780
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2024-02-26 22:16:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 //your JS code here. If required.
-let inputs = document.querySelectorAll(".code-container");
+let inputs = document.querySelectorAll("input");
+
+inputs.forEach((input,index)=>{
+	input.addEventListener("input",()=>{
+		if(index<inputs.length-1){
+			inputs[index+1].focus();
+		}
+	})
+	input.addEventListener("keydown",(event)=>{
+		if(event.key === "Backspace" && index>0){
+			inputs[index].value = '';
+			inputs[index-1].focus();
+		}
+	})
+});
+
 
 
